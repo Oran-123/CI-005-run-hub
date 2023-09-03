2d843eecaa9cd7a759aa2b3911ad2a56dd227aa2
Oran-123 <>
2023-09-03 13:25:11 +0000

    add product class to show fields for each product on the admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import Product, Category
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
 admin.site.register(Product)
 admin.site.register(Category)
