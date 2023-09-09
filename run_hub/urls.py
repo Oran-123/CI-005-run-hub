95d58149aa9b3e17a7e5ae71319c4898e71de1cd
Oran-123 <>
2023-09-09 18:37:44 +0000

    add path for bag app urls

--- run_hub/urls.py
+++ run_hub/urls.py
@@ -19,10 +19,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
