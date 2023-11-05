4055ebe19c3f0888c85d5eae6dccc7e7b4965622
Oran-123 <>
2023-11-05 14:49:19 +0000

    Set up the initial structure for the add_to_bag view

--- bag/views.py
+++ bag/views.py
@@ -7,3 +7,9 @@ def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request,item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
