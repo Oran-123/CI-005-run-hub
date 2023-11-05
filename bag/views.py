f8d485dc30493c596ed17c5232aa1da2afe01137
Oran-123 <>
2023-11-05 16:34:52 +0000

    update the add_to_bag view so it renders the bag template when called

--- bag/views.py
+++ bag/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
 )
 from django.contrib import messages
-from .models import Product
+from products.models import Product
 # Create your views here.
 
 
@@ -15,7 +15,6 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
     size = None
 
     if 'product_size' in request.POST:
@@ -46,15 +45,26 @@ def add_to_bag(request, item_id):
             messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
-    return redirect(redirect_url)ks
+    bag_items = request.session.get('bag', {})
+
+    context = {
+        'bag_items': bag_items,
+    }
+
+    return render(request, 'bag/bag.html', context)
+
 
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
 
-if 'product_size' in request.POST:
-    size = request.POST['product_size']
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
     bag = request.session.get('bag', {})
 
     if size:
@@ -81,7 +91,7 @@ if 'product_size' in request.POST:
             bag.pop(item_id)
             messages.success(request,
                             (f'Removed {product.name} '
-                            f'from your bag'))
-    
+                            f'from your bag'))  
+
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
