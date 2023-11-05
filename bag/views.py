e7f18036b1c13a6ced9c7f95aabc3461c123dcb1
Oran-123 <>
2023-11-05 14:51:23 +0000

    Update the shopping bag when product size is specified.

--- bag/views.py
+++ bag/views.py
@@ -19,3 +19,18 @@ def add_to_bag(request,item_id):
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
+
+    if size:
+    if item_id in list(bag.keys()):
+        if size in bag[item_id]['items_by_size'].keys():
+            bag[item_id]['items_by_size'][size] += quantity
+            messages.success(
+                request, (f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}'))
+        else:
+            bag[item_id]['items_by_size'][size] = quantity
+            messages.success(
+                request, (f'Added size {size.upper()} {product.name} to your bag'))
+    else:
+        bag[item_id] = {'items_by_size': {size: quantity}}
+        messages.success(
+            request, (f'Added size {size.upper()} {product.name} to your bag'))
